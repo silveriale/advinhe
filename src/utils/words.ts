cae7889161296a1163fa1624d21ffed9cb3acc5d
silveriale <leticia.silveria.costa@gmail.com>
2025-09-22 16:02:25 -0300

    primeiro commit

--- src/utils/words.ts
+++ src/utils/words.ts
@@ -4,6 +4,7 @@ export type Challenge = {
   tip: string;
 };
 
+
 export const WORDS: Challenge[] = [
   { id: 1, word: "CSS", tip: "Linguagem de estilos" },
   { id: 2, word: "REACT", tip: "Biblioteca para criar interfaces Web" },
