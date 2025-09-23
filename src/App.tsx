14993ec89c7a78f1263b47f380aabe615b14124f
silveriale <leticia.silveria.costa@gmail.com>
2025-09-23 16:26:05 -0300

    Estilizando Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styles from "./app.module.css";
 import { Tip } from "./components/Tip";
+import { Letter } from "./components/Letter";
 import { Header } from "./components/Header";
 
 export default function App() {
@@ -12,6 +13,14 @@ export default function App() {
         <Header current={5} max={10} onRestart={handleRestartGame} />
 
         <Tip tip="Uma das linguagens de programação mais utilizadas" />
+
+        <div className={styles.word}>
+          <Letter value="R" />
+          <Letter value="E" />
+          <Letter value="A" />
+          <Letter value="C" />
+          <Letter value="T" />
+        </div>
       </main>
     </div>
   );
