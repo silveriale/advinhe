1d8e00f7698bfcd6dc9f31513ad4feadb019f710
silveriale <leticia.silveria.costa@gmail.com>
2025-09-23 19:03:49 -0300

    Variações de Tamanho de Letra
    - como criar variações de letras em um componente
    
    -  propriedade opcional "size" para alternar entre letras grandes e pequenas

--- src/components/Letter/index.tsx
+++ src/components/Letter/index.tsx
@@ -1,7 +1,7 @@
 /**
  * @file Este arquivo define o componente `Letter`, responsável por exibir
  * um caractere dentro de um bloco estilizado.
- * É utilizado no jogo para mostrar letras fixas ou digitadas.
+ * É utilizado no jogo para mostrar letras fixas ou digitadas, em tamanho padrão ou reduzido.
  */
 
 import styles from "./styles.module.css";
@@ -9,21 +9,27 @@ import styles from "./styles.module.css";
 // Define o tipo das propriedades aceitas pelo componente
 type Props = {
   value?: string; // Valor da letra a ser exibida, opcional
+  size?: "default" | "small"; // Define o tamanho da letra: padrão ou reduzido
 };
 
 /**
  * Componente `Letter`.
  * Renderiza um bloco estilizado exibindo uma letra.
  *
- * @param {Props} props - Objeto com a propriedade `value`, que representa a letra exibida.
+ * @param {Props} props - Objeto com as propriedades:
+ *  - `value`: representa a letra exibida.
+ *  - `size`: determina o tamanho da letra ("default" ou "small").
  * @returns {JSX.Element} Estrutura JSX que renderiza a letra em tela.
  */
-export function Letter({ value = "" }: Props) {
+export function Letter({ value = "", size = "default" }: Props) {
   return (
-    <div className={styles.letter}>
-      {" "}
-      {/* Container estilizado que envolve a letra */}
-      <span>{value}</span>{" "}
+    // Container estilizado que aplica classes diferentes conforme o tamanho selecionado
+    <div
+      className={`
+    ${styles.letter}
+    ${size === "small" && styles.letterSmall}`}
+    >
+      <span>{value}</span>
       {/* Exibe o valor da letra dentro de uma tag <span> */}
     </div>
   );
--- src/components/LettersUsed/index.tsx
+++ src/components/LettersUsed/index.tsx
@@ -4,18 +4,26 @@
  */
 
 import styles from "./styles.module.css";
+import { Letter } from "../Letter";
 
 /**
  * Componente `LettersUsed`.
- * Renderiza uma seção que exibe o título "Letras utilizadas".
+ * Renderiza uma seção contendo o título "Letras utilizadas" e as letras já usadas.
  *
  * @returns {JSX.Element} Estrutura JSX que mostra a área destinada às letras usadas.
  */
 export function LettersUsed() {
   return (
-    // Div container com classe CSS para estilização
+    // Container principal da seção de letras utilizadas
     <div className={styles.lettersUsed}>
+      {/* Título da seção */}
       <h5>Letras utilizadas:</h5>
+
+      {/* Lista de letras já utilizadas, renderizadas para o tamanho pequeno*/}
+      <div>
+        <Letter value="X" size="small" />
+        <Letter value="R" size="small" />
+      </div>
     </div>
   );
 }
