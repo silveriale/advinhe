251d8615d537ffe4ae215cbddc012ddb6a2a6e79
silveriale <leticia.silveria.costa@gmail.com>
2025-09-26 11:45:59 -0300

    Fim do Jogo
    - identifica o término de um jogo utilizando o hook useEffect
    
    - setTimeout para exibir uma mensagem de vitória antes de reiniciar o jogo

--- src/App.tsx
+++ src/App.tsx
@@ -52,6 +52,7 @@ export default function App() {
     );
 
     if (exists) {
+      setLetter("")
       return alert("Você já utilizou a letra: " + value);
     }
 
@@ -68,9 +69,32 @@ export default function App() {
     setLetter("");
   }
 
+  function endGame(message: string) {
+    alert(message);
+    startGame();
+  }
+
   useEffect(() => {
     startGame();
   }, []);
+
+  useEffect(() => {
+    if (!challenge) {
+      return;
+    }
+
+    setTimeout(() => {
+      if (score === challenge.word.length) {
+        return endGame("Parabéns, você descobriu a palavra!");
+      }
+
+      const attemptLimit = challenge.word.length + ATTEMPTS_MARGIN;
+
+      if (lettersUsed.length === attemptLimit) {
+        return endGame("Que pena, você usou todas as tentativas");
+      }
+    }, 200);
+  }, [score, lettersUsed.length]);
 
   if (!challenge) {
     return;
